273dd5262db62bf6967ef3254a5e494de3780cbf
Kaushik Kumaran <kumarankaushik@gmail.com>
2020-11-20 09:34:45 +0400

    init

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter, Route} from 'react-router-dom'
+
+import 'normalize.css/normalize.css'
+import './styles/styles.scss'
+
+const ExpenseDashboardPage = () => {
+    <div>
+        This is from my dashboard.
+    </div>
+};
+
+const routes = (
+    <BrowserRouter>
+    <Route path="/" component={ExpenseDashboardPage}/>
+    </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'))
